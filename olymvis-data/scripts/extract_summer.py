3c8053be56d03f0da149acaec8cc693d4015c41d
hongtaoh <fhaohongtao@163.com>
2020-11-08 06:53:34 -0500

    rebuilding site Sun Nov  8 06:53:34 EST 2020

--- olymvis-data/scripts/extract_summer.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-data = pd.read_csv('https://raw.githubusercontent.com/rgriff23/Olympic_history/master/data/athlete_events.csv')
-summer = data[data.Season == "Summer"].to_csv("../output/summer.csv", index=False)
